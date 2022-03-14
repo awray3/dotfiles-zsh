3d37710f6deb2895590aa9ad863c3e1d88946c07
Andrew Wray <andrew.wray@fullpower.com>
2022-03-13 20:38:37 -0700

    Add jupyter-book aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,3 +10,15 @@ alias reloadvim="nvim +PackerSync"
 alias zshconf="vim $ZSH_DIR/zshrc"
 alias vimconf="vim ~/.config/neovim-dotfiles/init.lua"
 alias sshconf="vim ~/.ssh/config"
+
+## Opening file types
+alias -s md=code
+alias -s py=code
+alias -s ipynb=code
+
+## Jupyter book
+alias jbb="jupyter-book build"
+
+jbc () {
+    jupyter-book clean $1 --all
+}
