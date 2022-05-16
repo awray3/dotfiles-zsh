8115525525651c1b36d412a7a03bceefeedc7cca
Andrew Wray <andrewwray2718@gmail.com>
2022-05-16 13:53:49 -0700

    Add function for installing kernels

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -23,6 +23,8 @@ alias -s py=code
 alias -s ipynb=code
 
 # script for making new myst markdown files
+# idea for improvement: if conda env is active,
+# automatically use that kernel instead of asking.
 new_markdown () {
     $CONDA_PYTHON_EXE $ZSH_DIR/new_markdown.py $@
 }
@@ -31,6 +33,13 @@ alias nmd="new_markdown"
 alias js="jupytext --sync"
 
 alias new_project="cookiecutter gh:awray3/new-project-cookiecutter"
+
+# function/alias for installing the currently activated 
+# venv as a jupyter kernel. Note that the environment variable
+# CONDA_DEFAULT_ENV gives the name of the current conda env.
+kernel_install () {
+    ipython kernel install --user --name="$CONDA_DEFAULT_ENV"
+}
 
 alias ls="pls"
 alias l="ls"
