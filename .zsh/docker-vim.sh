effa562b56084741b88a77459d8bc660a9bfd4f9
Andrew Wray <andrewwray2718@gmail.com>
2023-01-30 09:34:50 -0800

    Update dvim script

--- .zsh/docker-vim.sh
+++ .zsh/docker-vim.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env /bin/bash
 
 # neovim config version
-NEOVIM_TAG=0.1.0
+NEOVIM_TAG=0.2.0
 
 SUB_NAME=$(basename $(pwd))
 
@@ -11,12 +11,25 @@ CONTAINER_NAME="neovim-${SUB_NAME}-$$"
 
 echo Launching neovim in container: ${CONTAINER_NAME//.}
 
-_vim () {
+function _vim () {
+    local tag=$NEOVIM_TAG
+    while [[ $# -gt 0 ]]; do
+      case $1 in
+        --tag)
+          tag=$2
+          shift 2
+          ;;
+        *)
+          break
+          ;;
+      esac
+    done
+
     docker run -it --rm \
       --name ${CONTAINER_NAME//.} \
       -w /edit \
       -v `pwd`:/edit \
-      awray3/neovim:$NEOVIM_TAG \
+      awray3/neovim:$tag \
       nvim "$@"
 }
 
